7fceb709927db014b44711d08667cbe436432210
Kotty <kotty@guns-n-girls.de>
2016-04-15 17:49:07 +0200

    spaces

--- manifests/config/service.pp
+++ manifests/config/service.pp
@@ -1,31 +1,31 @@
 define maxscale::config::service (
-	$router,
-	$servers = undef,
-	$router_options = undef,
-	$filters = undef,
-	$user = undef,
-	$passwd = undef,
-	$enable_root_user = 0,
-	$localhost_match_wildcard_host = 1,
-	$version_string='MaxScale',
-	$weightby = undef,
-	$auth_all_servers = 1,
-	$strip_db_esc = 1,
-	$optimize_wildcard = 1,
-	$retry_on_failure = 1,
-	$log_auth_warnings = 0,
-	$connection_timeout = undef,
-	$max_slave_connections = undef,
-	$max_slave_replication_lag = undef,
-	$use_sql_variables_in = undef,
+  $router,
+  $servers = undef,
+  $router_options = undef,
+  $filters = undef,
+  $user = undef,
+  $passwd = undef,
+  $enable_root_user = 0,
+  $localhost_match_wildcard_host = 1,
+  $version_string='MaxScale',
+  $weightby = undef,
+  $auth_all_servers = 1,
+  $strip_db_esc = 1,
+  $optimize_wildcard = 1,
+  $retry_on_failure = 1,
+  $log_auth_warnings = 0,
+  $connection_timeout = undef,
+  $max_slave_connections = undef,
+  $max_slave_replication_lag = undef,
+  $use_sql_variables_in = undef,
 ) {
-	if $router == undef {
-		fail("The Router Type must be set!")
-	}
-	concat::fragment{ "Service ${name}":
-		target  => $::maxscale::params::configfile,
-		content => template("maxscale/service.erb"),
-		order   => 03,
-	}
+  if $router == undef {
+    fail("The Router Type must be set!")
+  }
+  concat::fragment{ "Service ${name}":
+    target  => $::maxscale::params::configfile,
+    content => template("maxscale/service.erb"),
+    order   => 03,
+  }
 
 }
