8a1b24069e3fb0e53cf06f89eba70b3ae762c0f1
Philipp Frik <philipp.frik@qsc.de>
2021-01-12 17:22:31 +0100

    fix variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -27,103 +27,103 @@ class maxscale::config(
 ) {
 
   if $threads == undef {
-    $real_threads = $maxscale::maxscale::threads
+    $real_threads = $maxscale::threads
   } else {
     $real_threads = $threads
   }
 
   if $auth_connect_timeout == undef {
-    $real_auth_connect_timeout = $maxscale::maxscale::auth_connect_timeout
+    $real_auth_connect_timeout = $maxscale::auth_connect_timeout
   } else {
     $real_auth_connect_timeout = $auth_connect_timeout
   }
 
   if $auth_read_timeout == undef {
-    $real_auth_read_timeout = $maxscale::maxscale::auth_read_timeout
+    $real_auth_read_timeout = $maxscale::auth_read_timeout
   } else {
     $real_auth_read_timeout = $auth_read_timeout
   }
 
   if $auth_write_timeout == undef {
-    $real_auth_write_timeout = $maxscale::maxscale::auth_write_timeout
+    $real_auth_write_timeout = $maxscale::auth_write_timeout
   } else {
     $real_auth_write_timeout = $auth_write_timeout
   }
 
   if $ms_timestamp == undef {
-    $real_ms_timestamp = $maxscale::maxscale::ms_timestamp
+    $real_ms_timestamp = $maxscale::ms_timestamp
   } else {
     $real_ms_timestamp = $ms_timestamp
   }
 
   if $syslog == undef {
-    $real_syslog = $maxscale::maxscale::syslog
+    $real_syslog = $maxscale::syslog
   } else {
     $real_syslog = $syslog
   }
 
   if $maxlog == undef {
-    $real_maxlog = $maxscale::maxscale::maxlog
+    $real_maxlog = $maxscale::maxlog
   } else {
     $real_maxlog = $maxlog
   }
 
   if $log_warning == undef {
-    $real_log_warning = $maxscale::maxscale::log_warning
+    $real_log_warning = $maxscale::log_warning
   } else {
     $real_log_warning = $log_warning
   }
 
   if $log_notice == undef {
-    $real_log_notice = $maxscale::maxscale::log_notice
+    $real_log_notice = $maxscale::log_notice
   } else {
     $real_log_notice = $log_notice
   }
 
   if $log_info == undef {
-    $real_log_info = $maxscale::maxscale::log_info
+    $real_log_info = $maxscale::log_info
   } else {
     $real_log_info = $log_info
   }
 
   if $log_debug == undef {
-    $real_log_debug = $maxscale::maxscale::log_debug
+    $real_log_debug = $maxscale::log_debug
   } else {
     $real_log_debug = $log_debug
   }
 
   if $log_augmentation == undef {
-    $real_log_augmentation = $maxscale::maxscale::log_augmentation
+    $real_log_augmentation = $maxscale::log_augmentation
   } else {
     $real_log_augmentation = $log_augmentation
   }
 
   if $logdir == undef {
-    $real_logdir = $maxscale::maxscale::logdir
+    $real_logdir = $maxscale::logdir
   } else {
     $real_logdir = $logdir
   }
 
   if $datadir == undef {
-    $real_datadir = $maxscale::maxscale::datadir
+    $real_datadir = $maxscale::datadir
   } else {
     $real_datadir = $datadir
   }
 
   if $cachedir == undef {
-    $real_cachedir = $maxscale::maxscale::cachedir
+    $real_cachedir = $maxscale::cachedir
   } else {
     $real_cachedir = $cachedir
   }
 
   if $piddir == undef {
-    $real_piddir = $maxscale::maxscale::piddir
+    $real_piddir = $maxscale::piddir
   } else {
     $real_piddir = $piddir
   }
 
   if $configdir == undef {
-    $real_configdir = $maxscale::maxscale::configdir
+    $real_configdir = $maxscale::configdir
   } else {
     $real_configdir = $configdir
   }
