8735aa8927f8514d271c373ed851f050f7dcca72
Kotty <kotty@guns-n-girls.de>
2021-01-19 13:04:07 +0100

    Test Pre_Cond

---
+++ spec/classes/install/apt_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'maxscale::install::apt' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+			let(:pre_condition) do
+				<<-PRE_COND
+					include maxscale
+				PRE_COND
+
+      it { is_expected.to compile }
+    end
+  end
+end
