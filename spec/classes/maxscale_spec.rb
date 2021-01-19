177caa8af1e52cbcc17880b90c090c948f165060
Kotty <kotty@guns-n-girls.de>
2021-01-19 12:03:43 +0100

    added empty lines

--- spec/classes/maxscale_spec.rb
+++ spec/classes/maxscale_spec.rb
@@ -6,8 +6,10 @@ describe 'maxscale' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+
       if f[:os]['family'] == 'Debian'
         let('include maxscale::install')
+
       end
       it { is_expected.to compile }
     end
