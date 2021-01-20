bf3bcb8a769b02c15a3c17d7c353c1c70a924199
Kotty <kotty@guns-n-girls.de>
2021-01-20 08:00:15 +0100

    added yum repo

---
+++ manifests/install/yum.pp
@@ -0,0 +1,26 @@
+# @summary this class adds the YumRepo
+#
+# This class is used to create the yum repo to install the maxscale
+#
+# @example
+#   include maxscale::install::yum
+class maxscale::install::yum (
+      String $repository_base_url
+) {
+
+  if ($::architecture != 'amd64') {
+            fail('Architectures != amd64 are not supported by the maxscale package repository!')
+                
+  }
+
+  class {'yum':
+    repos => {
+      'mariadb-maxscale' => {
+        baseurl  => $repository_base_url,
+        enabled  => true,
+        gpgkey   => 'https://downloads.mariadb.com/MaxScale/MariaDB-MaxScale-GPG-KEY',
+        gpgcheck => '1'
+      }
+    }
+  }
+}
