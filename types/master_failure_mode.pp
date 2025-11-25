205f05a1952780545b804ffcc5e287b1695f01b3
Brideau, Patrick <patrick.brideau@teluq.ca>
2025-11-25 12:22:41 -0500

    fix: add types for previous commit

---
+++ types/master_failure_mode.pp
@@ -0,0 +1,2 @@
+type Maxscale::Master_failure_mode = Enum['fail_instantly','fail_on_write','error_on_write']
+
---
+++ types/slave_selection_criteria.pp
@@ -0,0 +1,2 @@
+type Maxscale::Slave_selection_criteria = Enum['least_current_operations', 'adaptive_routing', 'least_behind_master', 'least_router_connections', 'least_global_connections']
+
