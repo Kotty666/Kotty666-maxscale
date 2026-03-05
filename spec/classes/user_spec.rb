3a8d35646eaf19aa0dde2677e6f44a95849d5506
Kotty <kotty@guns-n-girls.de>
2026-03-05 08:59:25 +0100

    fix spec-tests

--- spec/classes/user_spec.rb
+++ spec/classes/user_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'maxscale' do
@@ -5,43 +7,18 @@ describe 'maxscale' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      # Minimal sinnvolle Parameter, falls dein module strikte Typen ohne Defaults hat
-      let(:params) do
-        {
-          package_name: 'maxscale',
-          package_version: 'present',
-          service_ensure: 'running',
-          service_enable: true,
-          service_name: 'maxscale',
-          manage_repo: false,
-          repo_version: '24.02',          # oder was du erwartest
-          repo_base_url: nil,
-          gpg_key_id: '0xDEADBEEF',        # dummy ok, wenn nicht validiert
-          config_dir: '/etc/maxscale',
-          config_file: 'maxscale.cnf',
-          config_owner: 'maxscale',
-          config_group: 'maxscale',
-          config_mode: '0640',
-          global_options: {},
-          servers: {},
-          monitors: {},
-          services: {},
-          listeners: {},
-          filters: {},
-          manage_dirs: false,
-          log_dir: '/var/log/maxscale',
-          data_dir: '/var/lib/maxscale',
-          cache_dir: '/var/cache/maxscale',
-          pid_dir: '/run/maxscale',
-          maxscale_user: 'maxscale',
-          maxscale_group: 'maxscale',
-          extra_config_files: {},
-        }
-      end
+      context 'with manage_user => true' do
+        let(:params) do
+          {
+            manage_user: true,  # <-- Das fehlende Flag
+          }
+        end
 
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_class('maxscale::user') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('maxscale::user') }
+        it { is_expected.to contain_user('maxscale') }
+        it { is_expected.to contain_group('maxscale') }
+      end
     end
   end
 end
-
